9370bd3495058dfbc934889dcb0a9e3d37e92411
raj <82250359+kamaraj12312@users.noreply.github.com>
2022-09-12 02:42:23 -0700

    wcf login pages

---
+++ wcf and login/mvc login/WebApplication1/Controllers/loginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class loginController : Controller
+    {
+
+        ServiceReference1.Service1Client ser = new ServiceReference1.Service1Client();
+        ServiceReference1.vlogin vlogin = new ServiceReference1.vlogin();
+        [HttpGet]
+
+        // GET: login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login(ServiceReference1.vlogin vlog)
+        {
+            string check = ser.UserLogin(vlog);
+            if (check == "true")
+            {
+                Session["Username"] = vlog.UserName.ToString();
+                return RedirectToAction("User", "Login");
+            }
+            else
+            {
+                TempData["error"] = "<script> alert('Incorrect Usrename or Password... Please try again')</script>";
+                return View();
+            }
+        }
+    }
+}
---
+++ wcf and login/wcf login/WcfService1/IService1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string UserLogin(vlogin login);
+
+        
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class vlogin
+    {
+        public string vusername;
+        public string vpassword;
+
+        [DataMember]
+        public string UserName
+        {
+            get { return vusername; }
+            set { vusername = value; }
+        }
+        [DataMember]
+        public string Password
+        {
+            get
+            { return vpassword; }
+            set { vpassword = value; }
+        }
+    }
+}
